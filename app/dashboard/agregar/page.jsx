8939a73218da70909aced74e8a00c7e9bc152dd2
jorwi jesus <jgjg.0503@gmail.com>
2024-01-04 19:04:19 -0400

    cambio camara

--- app/dashboard/agregar/page.jsx
+++ app/dashboard/agregar/page.jsx
@@ -4,28 +4,45 @@ import { useState, useEffect } from "react";
 import { VscDeviceCamera } from "react-icons/vsc";
 
 export default function AgregarPage() {
+  const videoRef = useRef(null);
+  const cont=false
   const [image, setImage] = useState(null);
 
-  const handleCapture = async () => {
+  const startCamera = async () => {
     try {
+      cont=true
       const mediaDevices = navigator.mediaDevices;
       const stream = await mediaDevices.getUserMedia({ video: true });
-      const videoTrack = stream.getVideoTracks()[0];
-      const imageCapture = new ImageCapture(videoTrack);
-      const photoBlob = await imageCapture.takePhoto();
-      const imageUrl = URL.createObjectURL(photoBlob);
-      setImage(imageUrl);
-      stream.getVideoTracks()[0].stop();
+      videoRef.current.srcObject = stream;
     } catch (error) {
-      console.error("Error capturing photo:", error);
+      console.error("Error starting camera:", error);
+      cont=false
     }
+  };
+
+  const capturePhoto = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    canvas.getContext("2d").drawImage(videoRef.current, 0, 0);
+    const imageUrl = canvas.toDataURL("image/jpeg");
+    setImage(imageUrl);
+    cont=false
   };
 
   return (
     <div className='flex justify-center flex-col items-center gap-y-4 mt-6'>
+      {cont &&
+        <video onClick={capturePhoto}
+          ref={videoRef}
+          autoPlay
+          className="w-52 h-40"
+          style={{ border: "1px solid #888" }}
+        />
+      }
       {image ?
         <img src={image} alt="Imagen" className='flex cursor-pointer h-40 w-52 border border-slate-500 bg-slate-50 justify-center items-center text-4xl text-slate-300' />
-        :<button name='capture' onClick={handleCapture} className='flex cursor-pointer h-40 w-52 border border-slate-500 bg-slate-50 justify-center items-center text-4xl text-slate-300'>
+        :<button name='capture' onClick={startCamera} className='flex cursor-pointer h-40 w-52 border border-slate-500 bg-slate-50 justify-center items-center text-4xl text-slate-300'>
           <VscDeviceCamera />
         </button>
       }
